229c2cd9ef173c215dbee9feb1234321b8ae8b15
Tommy Ly <tommy.ly@esendex.com>
2019-04-25 15:35:25 +0100

    first

---
+++ test.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+}
+
+resource "azurerm_resource_group" "rg" {
+        name = "testResourceGroup"
+        location = "westus"
+}
+
+resource "azurerm_template_deployment" "test" {
+  name                = "acctesttemplate-01"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  template_body = "${file("arm/az-maps-deploy.json")}"
+  deployment_mode = "Incremental"
+}
