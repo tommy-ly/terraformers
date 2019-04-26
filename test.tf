e59ffb13716c8bf057b45a292d85be44c257d574
Tommy Ly <tommy.ly@esendex.com>
2019-04-26 10:02:42 +0100

    adds some auth

--- test.tf
+++ test.tf
@@ -6,6 +6,25 @@ resource "azurerm_resource_group" "rg" {
         location = "westus"
 }
 
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_virtual_machine" "testiden" {
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+data "azurerm_builtin_role_definition" "contributor" {
+  name = "Contributor"
+}
+
+resource "azurerm_role_assignment" "testiden" {
+  name               = "${azurerm_virtual_machine.testiden.name}"
+  scope              = "${data.azurerm_subscription.primary.id}"
+  role_definition_id = "${data.azurerm_subscription.subscription.id}${data.azurerm_builtin_role_definition.contributor.id}"
+  principal_id       = "${lookup(azurerm_virtual_machine.testiden.identity[0], "principal_id")}"
+}
+
 resource "azurerm_template_deployment" "test" {
   name                = "acctesttemplate-01"
   resource_group_name = "${azurerm_resource_group.rg.name}"
