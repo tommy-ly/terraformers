576a7445d4d3a9427318493bc266a95197de1544
Tommy Ly <tommy.ly@esendex.com>
2019-04-29 11:55:43 +0100

    adds name back in

--- test.tf
+++ test.tf
@@ -4,13 +4,14 @@ terraform {
 
 provider "azurerm" {}
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "resourceGroup" {
     name     = "testResourceGroup"
     location = "ukwest"
 }
 
 resource "azurerm_template_deployment" "test" {
-    resource_group_name = "${azurerm_resource_group.rg.name}"
+    name                = "maps-template"
+    resource_group_name = "${azurerm_resource_group.resourceGroup.name}"
     template_body       = "${file("arm/az-maps-deploy.json")}"
     deployment_mode     = "Incremental"
 }
