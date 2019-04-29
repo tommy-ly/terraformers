5bf7c7b8615f4ef25a5eb3ff9d7eb30564fd79a7
Tommy Ly <tommy.ly@esendex.com>
2019-04-29 11:48:33 +0100

    removes template name and changes location

--- test.tf
+++ test.tf
@@ -5,13 +5,12 @@ terraform {
 provider "azurerm" {}
 
 resource "azurerm_resource_group" "rg" {
-    name = "testResourceGroup"
-    location = "westus"
+    name     = "testResourceGroup"
+    location = "ukwest"
 }
 
 resource "azurerm_template_deployment" "test" {
-    name                = "acctesttemplate-01"
     resource_group_name = "${azurerm_resource_group.rg.name}"
-    template_body = "${file("arm/az-maps-deploy.json")}"
-    deployment_mode = "Incremental"
+    template_body       = "${file("arm/az-maps-deploy.json")}"
+    deployment_mode     = "Incremental"
 }
